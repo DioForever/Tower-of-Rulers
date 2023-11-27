456c4618d846629d6312f5a240b06a3dc692b177
Doublepauld <97725022+Doublepauld@users.noreply.github.com>
2023-11-28 00:05:15 +0100

    lool

---
+++ Assets/dev-dom-w/FloorSetting.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class FloorSetting : MonoBehaviour 
+{
+    public float[] speedDebuffValues {get; private set;}
+
+    speedDebuffValues = {2.0f,0.5f};
+
+    dssd
+    
+}
--- Assets/dev-dom-w/StatModifier.cs
+++ Assets/dev-dom-w/StatModifier.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class StatModifier : MonoBehaviour
@@ -18,93 +19,105 @@ public class StatModifier : MonoBehaviour
     private float[] manaDebuffValues = {5.0f,0.25f,0.2f};
     private float originalMana;
 
-    public float floornumber = 1;
+    public int floornumber = 1;
     public bool BossIsDead = false;
 
     void Start()
     {
-        
-
         OriginalValues();
     }
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
-        if(BossIsDead == false && floornumber >= 1 && floornumber <= 10)
+        if (BossIsDead)
         {
-            ApplySpeedDebuff(speedDebuffValues[0]); // 50% rychlosti
-            ApplyHealthDebuff(healthDebuffValues[2]); // 2x života
-        }
-
-        else if(BossIsDead == false && floornumber >= 11 && floornumber <= 20)
-        {    
-            ApplyHealthDebuff(healthDebuffValues[0]); // 80% života
-            ApplyStrengthDebuff(strengthDebuffValues[2]); // 5x síly
-        }
-
-        else if(BossIsDead == false && floornumber >= 21 && floornumber <= 30)
-        {
-            ApplyStrengthDebuff(strengthDebuffValues[0]); //125% síly
-            ApplyManaDebuff(manaDebuffValues[2]); //5x many
-        }
-
-        else if(BossIsDead == false && floornumber >= 31 && floornumber <= 40)
-        {
-            horizontal = -horizontal;
-            vertical = -vertical;
-
-            ApplyHealthDebuff(healthDebuffValues[0]); //80% života
-            DisableMana(); //disable Mana usage
-        }
-
-        else if(BossIsDead == false && floornumber >= 41 && floornumber <= 50)
-        {
-            ApplySpeedDebuff(speedDebuffValues[1]);// 2x rychlosti
-
-             playerController.isCasting = false;//unbind skill
-        }
-
-        else if(BossIsDead == false && floornumber >= 51 && floornumber <= 60)
-        {
-            ApplyManaDebuff(manaDebuffValues[0]); // 20% many
-            ApplyStrengthDebuff(strengthDebuffValues[2]); // 5x síly
-        }
-
-        else if(BossIsDead == false && floornumber >= 61 && floornumber <=70)
-        {
-            ApplyHealthDebuff(healthDebuffValues[1]); // 50% života
-            
-            playerController.isDashing = false; //unbind dash
-        }
-
-        else if(BossIsDead == false && floornumber >= 71 && floornumber <= 80)
-        {
-            ApplyStrengthDebuff(strengthDebuffValues[1]); //2x síly
-            ApplyManaDebuff(manaDebuffValues[2]); // 5x many
-
-            horizontal = -horizontal;
-            vertical = -vertical;
-        }
-
-        else if(BossIsDead == false && floornumber >= 81 && floornumber <= 90)
-        {
-            ApplyManaDebuff(manaDebuffValues[1]); //4x many
-        }
-
-        else if(BossIsDead == false && floornumber >= 91 && floornumber <= 100)
-        {
-            ApplySpeedDebuff(speedDebuffValues[0]); // 50% ryhclosti
-            ApplyHealthDebuff(healthDebuffValues[0]); // 80% života
-            ApplyManaDebuff(manaDebuffValues[0]); // 20% many
+            RevertAllToOriginal();
         }
         else
         {
-           RevertAllToOriginal();
-        }
+            switch (floornumber)
+            {
+                case int n when n >= 1 && n <= 10:
 
-       
+                ApplySpeedDebuff(speedDebuffValues[0]); // 50% rychlosti
+                ApplyHealthDebuff(healthDebuffValues[2]); // 2x života
+
+                break;
+
+                case int n when n >= 11 && n <= 20:
+
+                ApplyHealthDebuff(healthDebuffValues[0]); // 80% života
+                ApplyStrengthDebuff(strengthDebuffValues[2]); // 5x síly
+
+                break;
+
+                case int n when n >= 21 && n <= 30:
+
+                ApplyStrengthDebuff(strengthDebuffValues[0]); //125% síly
+                ApplyManaDebuff(manaDebuffValues[2]); //5x many
+
+                break;
+
+                case int n when n >= 31 && n <= 40:
+
+                horizontal = -horizontal;
+                vertical = -vertical;
+
+                ApplyHealthDebuff(healthDebuffValues[0]); //80% života
+                DisableMana(); //disable Mana usage
+
+                break;
+
+                case int n when n >= 41 && n <= 50:
+
+                ApplySpeedDebuff(speedDebuffValues[1]);// 2x rychlosti
+
+                playerController.isCasting = false;//unbind skill
+
+                break;
+
+                case int n when n >= 51 && n <= 60:
+
+                ApplyManaDebuff(manaDebuffValues[0]); // 20% many
+                ApplyStrengthDebuff(strengthDebuffValues[2]); // 5x síly
+
+                break;
+
+                case int n when n >= 61 && n <= 70:
+
+                ApplyHealthDebuff(healthDebuffValues[1]); // 50% života
+                
+                playerController.isDashing = false; //unbind dash
+
+                break;
+
+                case int n when n >= 71 && n <= 80:
+
+                ApplyStrengthDebuff(strengthDebuffValues[1]); //2x síly
+                ApplyManaDebuff(manaDebuffValues[2]); // 5x many
+
+                horizontal = -horizontal;
+                vertical = -vertical;
+
+                break;
+
+                case int n when n >= 81 && n <= 90:
+
+                ApplyManaDebuff(manaDebuffValues[1]); //4x many
+
+                break;
+
+                case int n when n >= 91 && n <= 100:
+
+                ApplySpeedDebuff(speedDebuffValues[0]); // 50% ryhclosti
+                ApplyHealthDebuff(healthDebuffValues[0]); // 80% života
+                ApplyManaDebuff(manaDebuffValues[0]); // 20% many
+
+                break;
+            }
+            
+        }
     }
 
     // move speed debuffy
